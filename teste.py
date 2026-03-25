a429b7f797b54ea3e28cda910a83d06c10570254
arturguedees <rollembergartur@gmail.com>
2026-03-25 17:58:39 -0300

    Adição de novas funções / artur

--- teste.py
+++ teste.py
@@ -1,12 +1,30 @@
-# Função para calcular a vida restante após receber dano
+vida = 20
+
+# função para calcular a vida após receber dano
 def calcular_vida(vida, dano):
     nova_vida = vida - dano
+    if nova_vida<0:
+        nova_vida = 0  # a vida não pode ser negativa
     return nova_vida
 
-# Função para calcular a vida total após curar
+# função para verificar se o personagem está vivo
+def esta_vivo(vida):
+    if vida > 0:
+        return True
+    else:        
+        return False
+
+# função para calcular a vida total após curar
 def curar(vida, cura):
     nova_vida = vida + cura
     return nova_vida
 
-calcular_vida(10, 15)# Resultado: -5 (vida restante após receber dano)
-curar(10, 5) # Resultado: 15 (vida total após curar
+def caminho(lugar):
+    if lugar == "Floresta":
+        return "Você entrou na floresta. A brisa é limpa e gostosa, mas cuidado com os barulhos estranhos e as plantas venenosas!"
+    elif lugar == "Montanha":
+        return "Você subiu a montanha. A vista é incrível! Mas cuidado com os penhascos e os animais selvagens!"
+    elif lugar == "Caverna":
+        return "Você entrou na caverna. Frio, escura e úmida... o que será que tem lá dentro?"  
+    else:
+        return "Lugar desconhecido. Tente novamente."
